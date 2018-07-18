f8013cf3d7aebe4f8e0551515c9b993c521a9930
Luis Pabón <lpabon@gmail.com>
2018-07-18 16:23:29 -0400

    Updated

--- tutorial-python/setup-env.sh
+++ tutorial-python/setup-env.sh
@@ -1,8 +1,6 @@
 apt -y install jq && \
   pip install virtualenv && \
-  virtualenv sdk
-
-source sdk/bin/activate
-pip install grpcio grpcio-tools
-
-curl -L -O -s https://github.com/libopenstorage/openstorage/archive/master.zip
+  virtualenv sdk && \
+    source sdk/bin/activate && \
+    pip install grpcio grpcio-tools && \
+    curl -L -O -s https://github.com/libopenstorage/openstorage/archive/master.zip
