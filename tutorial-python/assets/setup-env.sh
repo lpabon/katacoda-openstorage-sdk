12d7257a20283321af85f51e2a6717309e2b0d09
Luis Pabón <luis@portworx.com>
2018-07-20 03:02:47 -0400

    Updated

--- tutorial-python/assets/setup-env.sh
+++ tutorial-python/assets/setup-env.sh
@@ -1,16 +1,28 @@
 VIRTUALENV="sdkenv"
 
+fail() {
+  echo "$1"
+  exit 1
+}
+
 echo "Install packages"
-apt -y install jq > /dev/null 2>&1
+apt -y install jq > /dev/null 2>&1 || fail "Failed to install packages"
 
 echo "Installing python virtual env"
-pip -q install virtualenv && \
-  virtualenv -q ${VIRTUALENV}&& \
-    source ${VIRTUALENV}/bin/activate && \
-    pip -q install grpcio grpcio-tools && \
-    curl -L -O -s https://github.com/libopenstorage/openstorage-sdk-clients/archive/master.zip && \
-    unzip -q master.zip 'openstorage-sdk-clients-master/sdk/python/*' && \
-    mv openstorage-sdk-clients-master/sdk/python/* . && \
-    rm -rf openstorage-master master.zip Makefile README.md DCO LICENSE
+echo "+ Installing virtualenv"
+pip -q install virtualenv || fail "Failed to install virtualenv"
+
+echo "+ Creating virtual environment in ${VIRTUALENV}"
+virtualenv -q ${VIRTUALENV} || fail "Failed to create virtualenv"
+source ${VIRTUALENV}/bin/activate
+
+echo "+ Installing grpc libraries in virtual environment"
+pip -q install grpcio grpcio-tools || fail "Failed to install grpc libraries"
+
+echo "+ Installing OpenStorage libraries"
+curl -L -O -s https://github.com/libopenstorage/openstorage-sdk-clients/archive/master.zip && \
+  unzip -q master.zip 'openstorage-sdk-clients-master/sdk/python/*' && \
+  mv openstorage-sdk-clients-master/sdk/python/* . && \
+  rm -rf openstorage-sdk-clients-master master.zip Makefile README.md DCO LICENSE
 
 echo "Done"
