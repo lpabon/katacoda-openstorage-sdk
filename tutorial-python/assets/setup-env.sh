9fe419ca8f84c12f92fcb1d141c3038fdac3a41b
Luis Pabón <luis@portworx.com>
2018-07-20 02:57:08 -0400

    Updated

--- tutorial-python/assets/setup-env.sh
+++ tutorial-python/assets/setup-env.sh
@@ -8,9 +8,9 @@ pip -q install virtualenv && \
   virtualenv -q ${VIRTUALENV}&& \
     source ${VIRTUALENV}/bin/activate && \
     pip -q install grpcio grpcio-tools && \
-    curl -L -O -s https://github.com/libopenstorage/openstorage/archive/master.zip && \
-    unzip -q master.zip 'openstorage-master/api/client/sdk/python/*' && \
-    mv openstorage-master/api/client/sdk/python/* . && \
-    rm -rf openstorage-master master.zip Makefile README.md
+    curl -L -O -s https://github.com/libopenstorage/openstorage-sdk-clients/archive/master.zip && \
+    unzip -q master.zip 'openstorage-sdk-clients-master/sdk/python/*' && \
+    mv openstorage-sdk-clients-master/sdk/python/* . && \
+    rm -rf openstorage-master master.zip Makefile README.md DCO LICENSE
 
 echo "Done"
