21d43d6ab134793810e02db618f6fa3735a13da3
Luis Pabón <luis@portworx.com>
2020-01-07 18:06:14 -0500

    save

--- tutorial-python/assets/setup-env.sh
+++ tutorial-python/assets/setup-env.sh
@@ -6,7 +6,9 @@ fail() {
 }
 
 echo "Install packages"
-apt update || fail "Failed to update apt"
+echo "+ Updating apt..."
+apt update > /dev/null 2>&1 || fail "Failed to update apt"
+echo "+ Installing python3..."
 apt -y install python3 python3-dev python3-venv jq > /dev/null 2>&1 || fail "Failed to install packages"
 
 echo "Installing python virtual env"
@@ -15,6 +17,6 @@ python3 -m venv ${VIRTUALENV} || fail "Failed to create virtualenv"
 source ${VIRTUALENV}/bin/activate
 
 echo "+ Installing libopenstorage-openstorage"
-pip3 install --user libopenstorage-openstorage || fail "Failed to install grpc libraries"
+pip3 install libopenstorage-openstorage || fail "Failed to install grpc libraries"
 
 echo "Done"
